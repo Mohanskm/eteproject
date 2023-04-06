b6306f70a356bf351016e9653603dbd553471472
Mohanskm <mohansk10086488@gmail.com>
2023-04-06 18:31:16 +0530

    Second Commit

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import find_packages,setup
+from typing import List
+
+Hyphen_e="-e ."
+def get_requirements(file_path:str)->List[str]:
+    '''
+    this function will return the list of requirements
+    '''
+    requirements=[]
+    with open(file_path) as file_obj:
+        requirements=file_obj.readlines()
+        [req.replace("\n", "")for req in requirements]
+        if Hyphen_e in requirements:
+            requirements.remove(Hyphen_e)
+    return requirements
+setup(
+name="mlproject",
+version="0.0.1",
+author="Mohan",
+author_email="mohansk10086488@gmail.com",
+packages=find_packages(),
+install_requires=get_requirements("requirements.txt")
+)
